893e3908c7e1069342c6134724d9306653fedaf0
Aubrey <51178662+FoxinTale@users.noreply.github.com>
2021-12-03 10:08:44 -0500

    Some Changes

---
+++ chardb/scripts/site.js
@@ -0,0 +1,64 @@
+window.onload = load_it();
+
+function menu() {
+    var x = document.getElementById("navmenu");
+    if (x.className === "sidenav") {
+        x.className += " responsive";
+    } else {
+        x.className = "sidenav";
+    }
+}
+
+function linkclick(url) {
+    jQuery(document).ready(function ($) {
+        $("#container").load(url);
+    });
+
+}
+
+function framerender(url) {
+    jQuery(document).ready(function ($) {
+        $("#container").load(url);
+    });
+}
+
+function load_it() {
+    jQuery(document).ready(function ($) {
+        $("#container").load("home.html");
+    });
+}
+
+
+
+function opennav() {
+  document.getElementById("sidenav").style.display = "block";
+  document.getElementById("overlay").style.display = "block";
+}
+
+function closenav() {
+  document.getElementById("sidenav").style.display = "none";
+  document.getElementById("overlay").style.display = "none";
+}
+
+window.onscroll = function() {scrolldo()};
+function scrolldo() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("top").classList.add("w3-card-4", "w3-animate-opacity");
+    document.getElementById("intro").classList.add("w3-show-inline-block");
+  } else {
+    document.getElementById("intro").classList.remove("w3-show-inline-block");
+    document.getElementById("top").classList.remove("w3-card-4", "w3-animate-opacity");
+  }
+}
+
+function dropdown(id) {
+  var x = document.getElementById(id);
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+    x.previousElementSibling.className += " w3-theme";
+  } else { 
+    x.className = x.className.replace("w3-show", "");
+    x.previousElementSibling.className = 
+    x.previousElementSibling.className.replace(" w3-theme", "");
+  }
+}
