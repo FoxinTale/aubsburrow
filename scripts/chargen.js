332f5052aa4afae688a208d08b30fe1cb71b1e2b
Aubrey <51178662+FoxinTale@users.noreply.github.com>
2026-02-26 12:05:31 -0500

    JS framework, added assets, changed link.
    Added more assets,  added new base, changed trello link, laid basic framework for javascripting.

---
+++ scripts/chargen.js
@@ -0,0 +1,5 @@
+function generateFirstLetter(){
+    // Generate a number between 1 and 26, and output it. This corresponds with a letter.
+    // This will probably be used twice. Once for first name, once for last name.
+}
+
---
+++ scripts/weightsystem.js
@@ -0,0 +1,11 @@
+function convertToMeters(){
+    // Takes an inch input and converts to meters.
+}
+
+function calculateBaseWeight(){
+    // Uses CDC formula, and calculates the base weight if the character was human in Kg.
+}
+
+function convertToPounds(){
+    // Converts Kg to Lbs.
+}
